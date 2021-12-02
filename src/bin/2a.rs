fd7f7f5e9ec60a3d8bce94ce69143b0cf1f4e25f
David Campion <David.Campion@walmartlabs.com>
2021-12-01 21:55:50 -0800

    spruce

--- src/bin/2a.rs
+++ src/bin/2a.rs
@@ -1,12 +1,12 @@
 use aoc2021::core::get_data;
-use std::iter::FromIterator;
 use std::convert::TryFrom;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 enum Direction {
     Forward,
     Down,
-    Up
+    Up,
 }
 
 impl TryFrom<&str> for Direction {
@@ -17,36 +17,36 @@ impl TryFrom<&str> for Direction {
             "forward" => Ok(Direction::Forward),
             "down" => Ok(Direction::Down),
             "up" => Ok(Direction::Up),
-            _ => Err(())
+            _ => Err(()),
         }
     }
 }
 
 #[derive(Debug)]
-struct Instruction (Direction, u32);
+struct Instruction(Direction, u32);
 
 impl Instruction {
     pub fn move_sub(&self, (horizontal, depth): (u32, u32)) -> (u32, u32) {
         match self.0 {
             Direction::Forward => (horizontal + self.1, depth),
             Direction::Up => (horizontal, depth - self.1),
-            Direction::Down => (horizontal, depth + self.1)
+            Direction::Down => (horizontal, depth + self.1),
         }
     }
 }
 
 impl<'a> FromIterator<&'a str> for Instruction {
-    fn from_iter<I: IntoIterator<Item=&'a str>>(iter: I) -> Self {
+    fn from_iter<I: IntoIterator<Item = &'a str>>(iter: I) -> Self {
         let mut i = iter.into_iter();
         Instruction(
             Direction::try_from(i.next().unwrap()).unwrap(),
-            i.next().unwrap().parse::<u32>().unwrap()
+            i.next().unwrap().parse::<u32>().unwrap(),
         )
     }
 }
 
 fn parse_line(line: &&str) -> Instruction {
-    line.split(" ").collect()
+    line.split(' ').collect()
 }
 
 fn into_instruction_set(raw: &[&str]) -> Vec<Instruction> {
@@ -54,19 +54,20 @@ fn into_instruction_set(raw: &[&str]) -> Vec<Instruction> {
 }
 
 fn follow_instructions(instructions: Vec<Instruction>) -> (u32, u32) {
-    instructions.iter().fold((0, 0), |acc, instruction| instruction.move_sub(acc))
+    instructions
+        .iter()
+        .fold((0, 0), |acc, instruction| instruction.move_sub(acc))
 }
 
 fn solve(raw: &[&str]) -> u32 {
     let (x, y) = follow_instructions(into_instruction_set(raw));
 
-    x*y
+    x * y
 }
 
-fn main(){
+fn main() {
     let input = get_data("2a");
-    let data: Vec<&str> = input.split("\n").collect();
-    
+    let data: Vec<&str> = input.split('\n').collect();
 
     println!("{}", solve(&data));
 }
@@ -83,7 +84,7 @@ mod day_2a_tests {
             "forward 8",
             "up 3",
             "down 8",
-            "forward 2"
+            "forward 2",
         ]);
 
         assert_eq!(result, 150);
